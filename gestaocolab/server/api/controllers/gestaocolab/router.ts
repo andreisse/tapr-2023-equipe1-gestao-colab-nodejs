4b7c15001fa4a3166bedbbc98712ebd0ee1f9e66
andreisse <86690998+andreisse@users.noreply.github.com>
2023-12-08 15:57:29 +0000

    gravando no banco

--- gestaocolab/server/api/controllers/gestaocolab/router.ts
+++ gestaocolab/server/api/controllers/gestaocolab/router.ts
@@ -1,6 +1,10 @@
 import express from 'express';
-import controller from './gestaocolabcontroller'
+import controller from './gestaocolabcontroller';
 
 export default express
     .Router()
-    .get('/', controller.all);
+    .get('/', controller.all)
+    .get('/:id', controller.getById)
+    .post('/', controller.post)
+    .put('/:id', controller.update)
+    .delete('/:id', controller.delete);
